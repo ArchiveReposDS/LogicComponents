9732b759f53b13e1de0ef01b67dd474a7d18bfcd
Dominik Swatek <dominik.swatek87@gmail.com>
2020-05-07 21:04:47 +0200

    DataBus

--- LogicComponents/1Program/Start.cs
+++ LogicComponents/1Program/Start.cs
@@ -25,107 +25,120 @@ namespace LogicComponents
             CounterClass.CounterCable = 0;
             CounterClass.CounterAction = 0;
 
+            DataBus DataBus = new DataBus();
+            DataBusHelper BusHelper = new DataBusHelper(DataBus);
+
+            BusHelper.PutDataToRegisterA(new byte[] {1,1,0,0,1,1,0,0});
+
+            Cable.Join(new Pin() { State = 1 }, DataBus.FromA);
+            BusHelper.SetRamAddress(63);  // 0 - 63
+            Cable.Join(new Pin() { State = 1 }, DataBus.ToRam);
+
+            Cable.Join(new Pin() { State = 1 }, DataBus.FromRam);
+            Cable.Join(new Pin() { State = 1 }, DataBus.ToB);
 
 
-            Ram8 r = new Ram8();
+            int k = 8;
 
-            DataBus Db = new DataBus();
-            Register registerA = new Register();
+            //Ram8 r = new Ram8();
 
-            Db.RegisterA = registerA;
+            
 
-            Cable.Join(new Pin() { State = 1 }, registerA.WriteEnable);
 
-            Cable.Join(new Pin() { State = 1 }, registerA.DataInput1);
-            Cable.Join(new Pin() { State = 1 }, registerA.DataInput2);
-            Cable.Join(new Pin() { State = 1 }, registerA.DataInput3);
-            Cable.Join(new Pin() { State = 0 }, registerA.DataInput4);
-            Cable.Join(new Pin() { State = 1 }, registerA.DataInput5);
-            Cable.Join(new Pin() { State = 1 }, registerA.DataInput6);
-            Cable.Join(new Pin() { State = 1 }, registerA.DataInput7);
-            Cable.Join(new Pin() { State = 1 }, registerA.DataInput8);
+            //Cable.Join(new Pin() { State = 1 }, Db.RegisterA.WriteEnable);
 
-            //z A do B
-            Cable.Join(new Pin() { State = 1 }, Db.FromA);
-            Cable.Join(new Pin() { State = 1 }, Db.ToB);
+            ////Cable.Join(new Pin() { State = 1 }, registerA.DataInput1);
+            ////Cable.Join(new Pin() { State = 1 }, registerA.DataInput2);
+            ////Cable.Join(new Pin() { State = 1 }, registerA.DataInput3);
+            ////Cable.Join(new Pin() { State = 0 }, registerA.DataInput4);
+            ////Cable.Join(new Pin() { State = 1 }, registerA.DataInput5);
+            ////Cable.Join(new Pin() { State = 1 }, registerA.DataInput6);
+            ////Cable.Join(new Pin() { State = 1 }, registerA.DataInput7);
+            ////Cable.Join(new Pin() { State = 1 }, registerA.DataInput8);
 
-            // z A do Ramu adres:111111 (64)
-            Cable.Join(new Pin() { State = 1 }, Db.FromA);
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn1);
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn2);
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn3);
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow1);
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow2);
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow3);
-            Cable.Join(new Pin() { State = 1 }, Db.ToRam);
+            ////z A do B
+            //Cable.Join(new Pin() { State = 1 }, Db.FromA);
+            //Cable.Join(new Pin() { State = 1 }, Db.ToB);
 
-            // z Ramu adres 000000 (0) do B - skasowanie B
-            Cable.Join(new Pin() { State = 0 }, Db.RamAddressColumn1);
-            Cable.Join(new Pin() { State = 0 }, Db.RamAddressColumn2);
-            Cable.Join(new Pin() { State = 0 }, Db.RamAddressColumn3);
-            Cable.Join(new Pin() { State = 0 }, Db.RamAddressRow1);
-            Cable.Join(new Pin() { State = 0 }, Db.RamAddressRow2);
-            Cable.Join(new Pin() { State = 0 }, Db.RamAddressRow3);
-            Cable.Join(new Pin() { State = 1 }, Db.FromRam);
+            //// z A do Ramu adres:111111 (64)
+            //Cable.Join(new Pin() { State = 1 }, Db.FromA);
+
+
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn1);
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn2);
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn3);
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow1);
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow2);
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow3);
+            //Cable.Join(new Pin() { State = 1 }, Db.ToRam);
+
+            //// z Ramu adres 000000 (0) do B - skasowanie B
+            //Cable.Join(new Pin() { State = 0 }, Db.RamAddressColumn1);
+            //Cable.Join(new Pin() { State = 0 }, Db.RamAddressColumn2);
+            //Cable.Join(new Pin() { State = 0 }, Db.RamAddressColumn3);
+            //Cable.Join(new Pin() { State = 0 }, Db.RamAddressRow1);
+            //Cable.Join(new Pin() { State = 0 }, Db.RamAddressRow2);
+            //Cable.Join(new Pin() { State = 0 }, Db.RamAddressRow3);
+            //Cable.Join(new Pin() { State = 1 }, Db.FromRam);
        
-            Cable.Join(new Pin() { State = 0 }, Db.ToB);
-            Cable.Join(new Pin() { State = 1 }, Db.ToB);
+            //Cable.Join(new Pin() { State = 0 }, Db.ToB);
+            //Cable.Join(new Pin() { State = 1 }, Db.ToB);
 
-            // z Ramu adres 11 do B - Przywrocenie stanu B
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn1);
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn2);
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn3);
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow1);
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow2);
-            Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow3);
-            Cable.Join(new Pin() { State = 0 }, Db.FromRam);
-            Cable.Join(new Pin() { State = 1 }, Db.FromRam);
-            Cable.Join(new Pin() { State = 0 }, Db.ToB);
-            Cable.Join(new Pin() { State = 1 }, Db.ToB);
-
-
-
-            int i1 = registerA.DataOutput1.State;
-            int i2 = registerA.DataOutput2.State;
-            int i3 = registerA.DataOutput3.State;
-            int i4 = registerA.DataOutput4.State;
-            int i5 = registerA.DataOutput5.State;
-            int i6 = registerA.DataOutput6.State;
-            int i7 = registerA.DataOutput7.State;
-            int i8 = registerA.DataOutput8.State;
+            //// z Ramu adres 11 do B - Przywrocenie stanu B
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn1);
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn2);
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressColumn3);
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow1);
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow2);
+            //Cable.Join(new Pin() { State = 1 }, Db.RamAddressRow3);
+            //Cable.Join(new Pin() { State = 0 }, Db.FromRam);
+            //Cable.Join(new Pin() { State = 1 }, Db.FromRam);
+            //Cable.Join(new Pin() { State = 0 }, Db.ToB);
+            //Cable.Join(new Pin() { State = 1 }, Db.ToB);
 
 
-            Cable.Join(new Pin() { State = 0 }, registerA.WriteEnable);
 
-            Cable.Join(new Pin() { State = 0 }, registerA.DataInput1);
-            Cable.Join(new Pin() { State = 0 }, registerA.DataInput2);
-            Cable.Join(new Pin() { State = 0 }, registerA.DataInput3);
-            Cable.Join(new Pin() { State = 0 }, registerA.DataInput4);
-            Cable.Join(new Pin() { State = 0 }, registerA.DataInput5);
-            Cable.Join(new Pin() { State = 0 }, registerA.DataInput6);
-            Cable.Join(new Pin() { State = 0 }, registerA.DataInput7);
-            Cable.Join(new Pin() { State = 0 }, registerA.DataInput8);
-
-            int q1 = registerA.DataOutput1.State;
-            int q2 = registerA.DataOutput2.State;
-            int q3 = registerA.DataOutput3.State;
-            int q4 = registerA.DataOutput4.State;
-            int q5 = registerA.DataOutput5.State;
-            int q6 = registerA.DataOutput6.State;
-            int q7 = registerA.DataOutput7.State;
-            int q8 = registerA.DataOutput8.State;
+            //int i1 = registerA.DataOutput1.State;
+            //int i2 = registerA.DataOutput2.State;
+            //int i3 = registerA.DataOutput3.State;
+            //int i4 = registerA.DataOutput4.State;
+            //int i5 = registerA.DataOutput5.State;
+            //int i6 = registerA.DataOutput6.State;
+            //int i7 = registerA.DataOutput7.State;
+            //int i8 = registerA.DataOutput8.State;
 
 
-            Cable.Join(new Pin() { State = 1 }, registerA.ReadEnable);
+            //Cable.Join(new Pin() { State = 0 }, registerA.WriteEnable);
 
-            int w1 = registerA.DataOutput1.State;
-            int w2 = registerA.DataOutput2.State;
-            int w3 = registerA.DataOutput3.State;
-            int w4 = registerA.DataOutput4.State;
-            int w5 = registerA.DataOutput5.State;
-            int w6 = registerA.DataOutput6.State;
-            int w7 = registerA.DataOutput7.State;
-            int w8 = registerA.DataOutput8.State;
+            //Cable.Join(new Pin() { State = 0 }, registerA.DataInput1);
+            //Cable.Join(new Pin() { State = 0 }, registerA.DataInput2);
+            //Cable.Join(new Pin() { State = 0 }, registerA.DataInput3);
+            //Cable.Join(new Pin() { State = 0 }, registerA.DataInput4);
+            //Cable.Join(new Pin() { State = 0 }, registerA.DataInput5);
+            //Cable.Join(new Pin() { State = 0 }, registerA.DataInput6);
+            //Cable.Join(new Pin() { State = 0 }, registerA.DataInput7);
+            //Cable.Join(new Pin() { State = 0 }, registerA.DataInput8);
+
+            //int q1 = registerA.DataOutput1.State;
+            //int q2 = registerA.DataOutput2.State;
+            //int q3 = registerA.DataOutput3.State;
+            //int q4 = registerA.DataOutput4.State;
+            //int q5 = registerA.DataOutput5.State;
+            //int q6 = registerA.DataOutput6.State;
+            //int q7 = registerA.DataOutput7.State;
+            //int q8 = registerA.DataOutput8.State;
+
+
+            //Cable.Join(new Pin() { State = 1 }, registerA.ReadEnable);
+
+            //int w1 = registerA.DataOutput1.State;
+            //int w2 = registerA.DataOutput2.State;
+            //int w3 = registerA.DataOutput3.State;
+            //int w4 = registerA.DataOutput4.State;
+            //int w5 = registerA.DataOutput5.State;
+            //int w6 = registerA.DataOutput6.State;
+            //int w7 = registerA.DataOutput7.State;
+            //int w8 = registerA.DataOutput8.State;
 
 
 
@@ -140,17 +153,17 @@ namespace LogicComponents
 
             // 1. zaladowac program do ramu
 
-            Ram8 ram = new Ram8();
-            RamHelper ramHelper = new RamHelper(ram);
+            //Ram8 ram = new Ram8();
+            //DataBusHelper ramHelper = new DataBusHelper(ram);
 
-            string ins1 = "mov eax, 3";
+            //string ins1 = "mov eax, 3";
 
 
-            byte[] value1 = new byte[] { 1, 0, 0, 0, 0, 0, 0, 1 };
-            byte[] value2 = new byte[] { 1, 0, 0, 1, 1, 0, 0, 1 };
+            //byte[] value1 = new byte[] { 1, 0, 0, 0, 0, 0, 0, 1 };
+            //byte[] value2 = new byte[] { 1, 0, 0, 1, 1, 0, 0, 1 };
 
-            ramHelper.Save(value1, 0);
-            var aa = ramHelper.Load(0);
+            //ramHelper.SaveInRam(value1, 0);
+            //var aa = ramHelper.LoadFromRam(0);
 
             // 2. uruchomic program
             // zegar tworzy impulsy, licznik impulsow uruchamia odpowiednia sciezke ktora wykonuje kroki programu
---
+++ LogicComponents/Helper/DataBusHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicComponents
+{
+    public class DataBusHelper
+    {
+        public DataBus DataBus { get; set; }
+
+        public DataBusHelper(DataBus dataBus)
+        {
+            this.DataBus = dataBus;
+        }
+
+
+        /// <summary>
+        /// Adress Range 0-63
+        /// </summary>
+        /// <param name="input"> 1 or 0 </param>
+        /// <param name="ramAddress"> 0 - 63 </param>
+        public void SetRamAddress(int ramAddress)
+        {
+            byte i4Row, i2Row, i0Row, i4Column, i2Column, i0Column;
+            ConvertAddressToBinary(ramAddress, out i4Row, out i2Row, out i0Row, out i4Column, out i2Column, out i0Column);
+
+            // SET ADDRESS
+            Cable.Join(new Pin() { State = i0Row }, DataBus.RamAddressRow1);
+            Cable.Join(new Pin() { State = i2Row }, DataBus.RamAddressRow2);
+            Cable.Join(new Pin() { State = i4Row }, DataBus.RamAddressRow3);
+            Cable.Join(new Pin() { State = i0Column }, DataBus.RamAddressColumn1);
+            Cable.Join(new Pin() { State = i2Column }, DataBus.RamAddressColumn2);
+            Cable.Join(new Pin() { State = i4Column }, DataBus.RamAddressColumn3);
+        }
+       
+
+        public void PutDataToRegisterA(byte[] input)
+        {
+            Cable.Join(new Pin() { State = 1 }, DataBus.RegisterA.WriteEnable);
+            Cable.Join(new Pin() { State = input[0] }, DataBus.RegisterA.DataInput1);
+            Cable.Join(new Pin() { State = input[1] }, DataBus.RegisterA.DataInput2);
+            Cable.Join(new Pin() { State = input[2] }, DataBus.RegisterA.DataInput3);
+            Cable.Join(new Pin() { State = input[3] }, DataBus.RegisterA.DataInput4);
+            Cable.Join(new Pin() { State = input[4] }, DataBus.RegisterA.DataInput5);
+            Cable.Join(new Pin() { State = input[5] }, DataBus.RegisterA.DataInput6);
+            Cable.Join(new Pin() { State = input[6] }, DataBus.RegisterA.DataInput7);
+            Cable.Join(new Pin() { State = input[7] }, DataBus.RegisterA.DataInput8);
+            Cable.Join(new Pin() { State = 0 }, DataBus.RegisterA.WriteEnable);
+        }
+
+
+        private static void ConvertAddressToBinary(int ramAddress, out byte i4Row, out byte i2Row, out byte i0Row, out byte i4Column, out byte i2Column, out byte i0Column)
+        {
+            if (ramAddress > 63)
+            {
+                Console.WriteLine("WRONG RAM ADDREESS - RAM ADDRESS BEETWEN 0 - 63");
+            }
+            // ram address 0- 63
+            i0Row = 0; i2Row = 0; i4Row = 0; i0Column = 0; i2Column = 0; i4Column = 0;
+            int row;
+            int column;
+
+            column = ramAddress/8;
+            row = ramAddress - (column * 8);
+
+
+            switch (row)
+            {
+                case 0: i0Row = 0; i2Row = 0; i4Row = 0; break;
+                case 1: i0Row = 1; i2Row = 0; i4Row = 0; break;
+                case 2: i0Row = 0; i2Row = 1; i4Row = 0; break;
+                case 3: i0Row = 1; i2Row = 1; i4Row = 0; break;
+                case 4: i0Row = 0; i2Row = 0; i4Row = 1; break;
+                case 5: i0Row = 1; i2Row = 0; i4Row = 1; break;
+                case 6: i0Row = 0; i2Row = 1; i4Row = 1; break;
+                case 7: i0Row = 1; i2Row = 1; i4Row = 1; break;
+            }
+
+            switch (column)
+            {
+                case 0: i0Column = 0; i2Column = 0; i4Column = 0; break;
+                case 1: i0Column = 1; i2Column = 0; i4Column = 0; break;
+                case 2: i0Column = 0; i2Column = 1; i4Column = 0; break;
+                case 3: i0Column = 1; i2Column = 1; i4Column = 0; break;
+                case 4: i0Column = 0; i2Column = 0; i4Column = 1; break;
+                case 5: i0Column = 1; i2Column = 0; i4Column = 1; break;
+                case 6: i0Column = 0; i2Column = 1; i4Column = 1; break;
+                case 7: i0Column = 1; i2Column = 1; i4Column = 1; break;
+            }
+        }
+    }
+}
